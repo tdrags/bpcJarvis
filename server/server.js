cd5792c1d2e8a5658984c82ccac86109c77b80d2
tdrags <tim.drager@gmail.com>
2023-01-16 22:15:46 -0500

    first commit

--- server/server.js
+++ server/server.js
@@ -24,18 +24,18 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
-        
+        console.log(prompt);
         //get response from Open API
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 20,
+            temperature: 0.7,
             max_tokens: 100,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0.0,
         });
-
+        console.log(response);
         res.status(200).send({
             bot:response.data.choices[0].text
         });
@@ -43,7 +43,7 @@ app.post('/', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).send(error || 'Something went wrong');
-    }cd
+    }
 })
 
 app.listen(5000, () => console.log('Server is running on port http://localhost:5000'))
